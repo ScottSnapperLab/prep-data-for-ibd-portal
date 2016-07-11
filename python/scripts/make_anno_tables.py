6e3204a514f6148648ae7f733a6d59c7c96c26f7
Gus Dunn <wadunn83@gmail.com>
2016-07-11 16:55:42 -0400

    ready for more files from Michael \

--- python/scripts/make_anno_tables.py
+++ python/scripts/make_anno_tables.py
@@ -7,19 +7,16 @@ import pandas as pd
 
 from python.functions import bool_to_int
 
-log_form = '%(asctime)s\t%(levelname)s\t%(name)s\t%(message)s'
-
-# TODO: Fix so that each XLS file's logs are grouped together instead of interleaved
-logging.basicConfig(filename=snakemake.log.path,
-                    format=log_form,
-                    datefmt='%Y-%m-%dT%H:%M:%S',
-                    level=logging.INFO)
+# try:
+#     snakemake = snakemake
+# except NameError:
+#     snakemake = None
 
 
-fam_name = snakemake.wildcards.fam_name
 
-exe = logging.getLogger(fam_name)
-exe.info("-- BEGIN make_anno_tables_1. --")
+
+
+
 
 ######## functions
 
@@ -60,81 +57,61 @@ convsns = {'BAM CLEAR': bool_to_int,}
 
 
 
-######## params
-# Columms we want mapped to new names we give them
-nmap = snakemake.params.nmap
-# Lines to add to the VCF headers that define the data in the annotation table we will write
-headers = snakemake.params.headers
 
-######## input
+def main():
+    """Do the main logic."""
+    log_form = '%(asctime)s\t%(levelname)s\t%(name)s\t%(message)s'
 
-xls = snakemake.input.xls
+    # TODO: Fix so that each XLS file's logs are grouped together instead of interleaved
+    logging.basicConfig(filename=snakemake.log.path,
+                        format=log_form,
+                        datefmt='%Y-%m-%dT%H:%M:%S',
+                        level=logging.INFO)
 
+    fam_name = snakemake.wildcards.fam_name
 
-######## output
+    exe = logging.getLogger(fam_name)
+    exe.info("-- BEGIN make_anno_tables_1. --")
 
-anno_table_path = snakemake.output.anno_table_path
-anno_headers_path = snakemake.output.anno_headers_path
-# anno_col_name_path = snakemake.output.anno_col_name_path
+    ######## params
+    # Columms we want mapped to new names we give them
+    nmap = snakemake.params.nmap
+    # Lines to add to the VCF headers that define the data in the annotation table we will write
+    headers = snakemake.params.headers
 
+    ######## input
 
-######## business
-
-exe.info("Converting, extracting and combining columns as needed.")
-df = extract_columns(xls=xls, nmap=nmap, converters=convsns)
-
-exe.info("Writing table to file.")
-df.to_csv(anno_table_path, sep='\t', index=False)
-#
-# with open(anno_col_name_path,'w') as col_names:
-#     col_names.write(','.join(df.columns.values))
-
-exe.info("Writing headers to file.")
-with open(anno_headers_path, 'w') as headers_out:
-    for h in headers:
-        headers_out.write("{header_line}\n".format(header_line=h))
+    xls = snakemake.input.xls
 
 
-# ############################# bgzip #############################
-# exe.info("Compressing table with bgzip.")
-# # build the command and break into its parts
-# bgzip_cmd = lex.split("bgzip -c {csv} > {bgz}".format(csv=lex.quote(anno_table_path),
-#                                                       bgz=lex.quote(anno_table_path_bgz)
-#                                                       )
-#                       )
-# # Run the bgzip command
-# bgz = sbp.run(bgzip_cmd,
-#               stdout=sbp.PIPE,
-#               stderr=sbp.PIPE,
-#               universal_newlines=True
-#               )
-#
-# exe.info(" ".join(bgzip_cmd))
-# exe.info("SEE BELOW:\n{div}STDOUT{div}\n{sdo}\n{div}STDERR{div}\n{sde}".format(div="-"*30,
-#                                                                              sdo=bgz.stdout,
-#                                                                              sde=bgz.stderr)
-#        )
-#
-#
-#
-# ############################# tabix #############################
-# exe.info("Indexing table with tabix.")
-# # build the command and break into its parts
-# tabix_cmd = lex.split("tabix -s1 -b2 -e2 -S1 {bgz}".format(bgz=lex.quote(anno_table_path_bgz)
-#                                                       )
-#                       )
-#
-# # Run the tabix command
-# tbx = sbp.run(tabix_cmd,
-#               stdout=sbp.PIPE,
-#               stderr=sbp.PIPE,
-#               universal_newlines=True
-#               )
-#
-# exe.info(" ".join(tabix_cmd))
-# exe.info("SEE BELOW:\n{div}STDOUT{div}\n{sdo}\n{div}STDERR{div}\n{sde}".format(div="-"*30,
-#                                                                                   sdo=tbx.stdout,
-#                                                                                   sde=tbx.stderr)
-#          )
+    ######## output
 
-exe.info("-- END make_anno_tables_1. --")
+    anno_table_path = snakemake.output.anno_table_path
+    anno_headers_path = snakemake.output.anno_headers_path
+    # anno_col_name_path = snakemake.output.anno_col_name_path
+
+
+    ######## business
+
+    exe.info("Converting, extracting and combining columns as needed.")
+    df = extract_columns(xls=xls, nmap=nmap, converters=convsns)
+
+    exe.info("Writing table to file.")
+    df.to_csv(anno_table_path, sep='\t', index=False)
+    #
+    # with open(anno_col_name_path,'w') as col_names:
+    #     col_names.write(','.join(df.columns.values))
+
+    exe.info("Writing headers to file.")
+    with open(anno_headers_path, 'w') as headers_out:
+        for h in headers:
+            headers_out.write("{header_line}\n".format(header_line=h))
+
+
+    exe.info("-- END make_anno_tables_1. --")
+
+
+
+main()
+# if __name__ == '__main__':
+#     main()
